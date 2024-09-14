00fd09fec3f8ed17de17060faabeeee161307241
Krzysztof Gorzelak <krzysztof.gorzelak@gmail.com>
2024-09-14 19:08:22 +0200

    Updating the contact information

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Mopidy-HomeAssistant',
+    name='mopidy-homeassistant-mixer',
     version='0.1.0',
-    url='https://github.com/yourusername/mopidy-homeassistant',
-    license='Apache License, Version 2.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='Mopidy extension to control volume and mute through Home Assistant.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    description='Mopidy mixer for Home Assistant media player control',
+    author='Krzysztof Gorzelak',
+    author_email='gorzelak@users.noreply.github.com',  # GitHub noreply email
+    url='https://github.com/gorzelak/mopidy-homeassistant-mixer',
     packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
     install_requires=[
-        'Mopidy >= 3.0',
+        'mopidy>=3.0',
         'requests',
+        'websockets',
     ],
     entry_points={
-       'mopidy.ext': [
-            'homeassistant = mopidy_homeassistant.extension:Extension',
+        'mopidy.ext': [
+            'homeassistant = mopidy_homeassistant:Extension',
         ],
     },
 )
